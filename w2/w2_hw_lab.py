dca5fa521536f5fb5481e61ace3fddfd41498c82
Charles-NEIT <cloconnellv@email.neit.edu>
2025-01-13 12:58:28 -0500

    Update w2_hw_lab.py

--- w2/w2_hw_lab.py
+++ w2/w2_hw_lab.py
@@ -3,7 +3,8 @@
 #w2d2 HW lab   
 #1-14-2025 W2D2
 
-#program prompt:
+#program prompt: This program reads a csv file and then lists all the computers in the csv file filehandling.csv.
+
 
 
 
@@ -23,3 +24,47 @@ import csv
 
 
 
+#------------------------------------Main Code-------------------------------------------
+
+#header
+print(f"{'Type':10}\t {'Brand':6}\t   {'Cpu':4} \t {'Ram':10} {'1st disk':10} {'No HDD':10} {'2nd disk':10} {'Os':10} {'Year':10}")
+
+#opening csv file
+
+with open ("w2/filehandling.csv") as csvfile:
+
+    file = csv.reader(csvfile)
+
+    for record in file:
+        #make variables for items in row of csv files
+        computer_type = record[0]
+        brand = record[1]
+        cpu_iteration = record[2]
+        ram = record[3]
+        first_disk = record[4]
+        no_hdd = record[5]
+        #second_disk = record[6]
+        #os = record[7]
+        #year = record[8]
+        
+        
+        
+        #turning records into output values 
+        #getting computer type 
+        if computer_type == "L":
+            computer_type = "Labtop"
+        elif computer_type == "D":
+            computer_type = "Desktop"
+        
+        
+        
+        #getting the brand 
+        if brand =="DL":
+            brand = "Dell"
+        elif brand == "HP":
+            brand == "HP"
+        elif brand == "GW":
+            brand = "Gateway"
+
+
+        print(f"{computer_type:16} {brand:3}")
