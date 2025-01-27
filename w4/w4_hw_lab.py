104e3a1baf2457a30a7de168d1e53ecfda4293ad
Charles-NEIT <cloconnellv@email.neit.edu>
2025-01-27 08:44:54 -0500

    added w4 things

---
+++ w4/w4_hw_lab.py
@@ -0,0 +1,29 @@
+#Charles O'Connell
+#SE126.02
+#w4 hw lab 
+#1/27/2025
+
+#prompt:
+
+#var library 
+
+
+
+
+
+#imports
+
+
+
+#functions
+
+
+
+
+
+
+
+
+
+
+#----------------------main code--------------------
---
+++ w4/w4_in_class_lab.py
@@ -0,0 +1,29 @@
+#Charles O'Connell
+#SE126.02
+#w4 in class lab 
+#1/27/2025
+
+#prompt:
+
+#var library 
+
+
+
+
+
+#imports
+
+
+
+#functions
+
+
+
+
+
+
+
+
+
+
+#----------------------main code--------------------
